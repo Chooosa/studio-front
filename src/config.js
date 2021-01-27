1c9147939b241f098f495c1b59bd89c59e2d8504
Ait0ne666 <72990001+Ait0ne666@users.noreply.github.com>
2021-01-27 20:36:37 +0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const CMS_URL = 'http://strapi.lilekov-studio.com/';
+export const CMS_URL = 'https://strapi.lilekov-studio.com/';
 export const API_URL = process.env.NODE_ENV==='production'? 'https://lilekov-studio.com/api/' :''
